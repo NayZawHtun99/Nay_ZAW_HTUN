8d49205e4b8ca4c8951716661837f325874b7192
Nay Zaw Htun <nhtun@student.hult.edu>
2025-01-29 18:58:17 +0000

    Update game Name

---
+++ tip/tip.py
@@ -0,0 +1,14 @@
+def dollars_to_float(d):
+    d=d.replace("$","")
+    return float(d)
+def percent_to_float(p):
+    p=p.replace("%","")
+    p=float(p) 
+    return p
+
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent/100
+    print(f"Leave ${tip:.2f}")
+main()
